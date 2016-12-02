1cd5f9fd8a7cc36273c6261b9c4399e78172bfed
ABCanG <abcang1015@gmail.com>
2016-12-03 01:33:29 +0900

    init

---
+++ client/src/js/funclub.js
@@ -0,0 +1,155 @@
+$(function() {
+  FastClick.attach(document.body);
+
+  var socket = io();
+
+  var jrumbleOption = {
+    x: 1,
+    y: 1,
+    rotation: 2,
+    speed: 50
+  };
+
+  function Character(name) {
+    this.timeout = null;
+    this.count = 0;
+    this.msg_selector = '.' + name + ' .msg';
+    this.count_selector = '.' + name + ' .count';
+    this.button_selector = '.' + name + ' button';
+
+    $(this.msg_selector).jrumble(jrumbleOption);
+    $(this.count_selector).jrumble(jrumbleOption);
+
+    $(this.button_selector).on('click', function() {
+      socket.emit('love', { type: name });
+    });
+  }
+
+  Character.prototype.setCount = function(count) {
+    this.count = count;
+    $(this.count_selector).text(count);
+  };
+
+  Character.prototype.increment = function() {
+    this.setCount(this.count + 1);
+    if (!this.timeout) {
+      $(this.msg_selector)
+        .stop()
+        .animate({ opacity: 1 }, 0)
+        .trigger('startRumble');
+      $(this.count_selector).trigger('startRumble');
+    }
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout((function() {
+      $(this.msg_selector)
+        .animate({ opacity: 0 }, 1000)
+        .trigger('stopRumble');
+      $(this.count_selector).trigger('stopRumble');
+      this.timeout = null;
+    }).bind(this), 1000);
+  };
+
+  var conoha = new Character('conoha');
+  var anzu = new Character('anzu');
+
+  socket.on('init', function(data) {
+    conoha.setCount(data.conoha);
+    anzu.setCount(data.anzu);
+  });
+
+  socket.on('love', function(data) {
+    switch (data.type) {
+      case 'conoha':
+        conoha.increment();
+        break;
+
+      case 'anzu':
+        anzu.increment();
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  socket.on('visitor', function(data) {
+    $('.visitor-count').text(data.count);
+  });
+
+  console.log('あなたの愛はわかりますが、一度に100以上送るのはやめましょう。');
+});
+
+
+// contributed by がおさん(@gaogao_9)
+
+function MikumoCPSCounterClass() {
+  var cnt = [];
+  var ele = $('.count');
+  var name = $('.name').map(function(i, e) { return $(e).text().match(/[^\s]+/)[0]; });
+  var PAD = function(text, num) {
+    for (var i = num; i--;) text = '0' + text;
+    return text.slice(-num);
+  };
+
+  for (var i = ele.length; i--;) cnt.push([]);
+
+  var self = this;
+  var LOOP = function(text, flag, date) {
+    text = [];
+    flag = false;
+    date = new Date();
+    ele.each(function(i, e) {
+      cnt[i].unshift($(e).text() - 0);
+      if (cnt[i].length < 2) {
+        flag = true;
+        return;
+      } else if (cnt[i].length > 5) {
+        cnt[i].length = 5;
+      }
+      text[i] = name[i] + 'の秒間カウント：' + ~~(self.getCurrentCPS(i)) + '回';
+    });
+    if (!flag) {
+      text = text.join(',');
+      text = '[' + PAD(date.getHours(), 2) + ':' + PAD(date.getMinutes(), 2) + ':' + PAD(date.getSeconds(), 2) + ']' + text;
+      if (self.isShowLog) console.log(text);
+    }
+  };
+
+  var timerId = null;
+  self.isShowLog = true;
+  self.start = function() {
+    if (timerId != null) return;
+    timerId = setInterval(LOOP, 1000);
+  };
+  self.stop = function() {
+    if (timerId == null) return;
+    clearInterval(timerId);
+    timerId = null;
+  };
+  self.getTimerId = function() { return timerId; };
+  self.getCurrentCPS = function(i) {
+    if (timerId == null) return 1;
+    if (!isNaN(i) && i !== null) {
+      if (!cnt[i] || cnt[i].length == 1) return -1;
+      return (cnt[i][0] - cnt[i][cnt[i].length - 1]) / (cnt[i].length - 1);
+    } else {
+      return cnt.map(function(e, i) {
+        if (e.length < 2) return -1;
+        return (e[0] - e[e.length - 1]) / (e.length - 1);
+      });
+    }
+  };
+}
+
+var mikumo;
+
+$(function() {
+  mikumo = new MikumoCPSCounterClass();
+});
+
+// example
+// mikumo.isShowLog = false; //計測中にコンソールにログを吐くかを指定します(bool型)。デフォルトはtrueです。
+// mikumo.start(); //計測を開始します
+// mikumo.getTimerId(); //ループに使うタイマーidを参照します(読み取り専用)
+// mikumo.getCurrentCPS(); //現在のCPS(Click Per Second)を表示します(引数なし:二人共,引数0:このはちゃんのみ,引数1:あんずちゃんのみ)
+// mikumo.stop(); //計測をやめます
---
+++ server.js
@@ -0,0 +1,59 @@
+const Koa = require('koa');
+const path = require('path');
+const serve = require('koa-static');
+const c2k = require('koa-connect');
+const harp = require('harp');
+const Redis = require('ioredis');
+
+const port = (process.env.REDIS_PORT && process.env.REDIS_PORT.includes(':') ?
+  Number(process.env.REDIS_PORT.split(':').pop()) : // for Docker
+  process.env.REDIS_PORT) || 6379;
+const host = (process.env.REDIS_NAME && process.env.REDIS_NAME.split('/').pop()) || // for Docker
+  process.env.REDIS_HOST || 'localhost';
+
+const redisOption = {
+  db: process.env.REDIS_DB || 0,
+  port,
+  host,
+};
+
+const redis = new Redis(redisOption);
+const app = new Koa();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(serve(path.resolve('./public')));
+} else {
+  console.log('development mode');
+  app.use(serve(path.resolve('./client/src')));
+  app.use(c2k(harp.mount(path.resolve('./client/src'))));
+}
+
+const server = app.listen(process.env.PORT || 3000);
+const io = require('socket.io').listen(server);
+
+io.on('connection', async (socket) => {
+  io.emit('visitor', socket.client.conn.server.clientsCount);
+
+  const [anzuCount, conohaCount] = (await redis.multi()
+    .get('count:anzu')
+    .get('count:conoha')
+    .exec()).map((res) => res[1]);
+
+  // 初期データ配信
+  await socket.emit('init', {
+    anzu: Number(anzuCount),
+    conoha: Number(conohaCount)
+  });
+
+  socket.on('love', (data) => {
+    const {type} = data;
+    if (type === 'anzu' || type === 'conoha') {
+      redis.incr(`count:${type}`);
+      io.emit('love', {type});
+    }
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('visitor', socket.client.conn.server.clientsCount);
+  });
+});
